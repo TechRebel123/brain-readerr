5cffa49d4ff96f58ca8da431d632839ee8350fe9
TechRebel <145196893+TechRebel123@users.noreply.github.com>
2023-09-23 15:22:44 +0530

    Add files via upload

---
+++ brain reader.py
@@ -0,0 +1,19 @@
+import time
+
+number = int(input("Enter the number you are thinking of:"))
+
+time.sleep(5)
+
+print("analyzing possibilities")
+
+time.sleep(6)
+
+print("calculating based on your recent activity!")
+
+time.sleep(5)
+
+print("almost done")
+
+time.sleep(5)
+
+print("The number you were thinking of is",number)
